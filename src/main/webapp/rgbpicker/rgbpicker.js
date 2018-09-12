a4496a6f76e6cdd5331b1d460e5de7f2f449df48
rickycraft <ricky.roveri@gmail.com>
2018-09-12 14:20:03 +0200

    fixed rgb slider enabled

--- src/main/webapp/rgbpicker/rgbpicker.js
+++ src/main/webapp/rgbpicker/rgbpicker.js
@@ -3,7 +3,7 @@ app.controller("RgbCtrl", RgbCtrl);
 function RgbCtrl($scope){
 	
 	$scope.rgbEnabled = function(){
-		if ($scope.isOn && $scope.fadeStatus == "off"){
+		if ($scope.isOn && $scope.fadeStatus == "on"){
 			return true;
 		} else {
 			return false;
