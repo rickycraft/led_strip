3bc9e48328e108f7b232337e65bcc59fb833ee50
rickycraft <ricky.roveri@gmail.com>
2019-01-20 21:52:57 +0100

    return json instead of html

--- Arduino/arduino_wifi_ledstrip/arduino_wifi_ledstrip.ino
+++ Arduino/arduino_wifi_ledstrip/arduino_wifi_ledstrip.ino
@@ -1,12 +1,13 @@
 //-----------WiFi Settings-----------
 #include <ESP8266WiFi.h>
 #include <math.h>
-const char* ssid = "TP-LINK";      
-const char* wifiPass = "";    
+#include <ArduinoJson.h>
+const char* ssid = "TP-LINK";
+const char* wifiPass = "";
 WiFiServer server(80);
 //the three next lines are for static IP configuration
 //if you activate this remember to uncomment the WiFi.config line in the setupWifi() function
-IPAddress ip(192,168,1,220);       //static IP adress of device 
+IPAddress ip(192,168,1,220);       //static IP adress of device
 IPAddress gateway(192,168,1,1);   //gateway
 IPAddress subnet(255,255,255,0);  //network mask
 
@@ -16,87 +17,109 @@ IPAddress subnet(255,255,255,0);  //network mask
 //-----------LED Settings-----------
 const int LED_COUNT = 3;      //3 for RGB leds, 5 for RGB + Cold and Warm leds
 //arrays are set up to hold the values in the order: red blue green cold warm
-const int ledPin[LED_COUNT] = {14, 13, 12}; //D5 D7 D6 on weMos d1 mini
+const int ledPin[LED_COUNT] = {14, 5, 4}; //D5 D6 D7 on weMos d1 mini
 int ledCurrentVal[LED_COUNT] = {0, 0, 0};   //the current value of leds
 int ledFadeTo[LED_COUNT] = {0, 0, 0};       //the final value leds are fading towards
+unsigned long currTime = 0;
+int unavailableCount = 0;
 
 //-----------Other Variables-----------
 bool ewStatus = false;
-const int ewPin = 5;
+const int ewPin = 13; //D8
 
-void setup() 
+//-------JSON-------
+const int bufferLen = JSON_OBJECT_SIZE(3);
+StaticJsonBuffer<100> jsonBuffer;
+JsonObject& root = jsonBuffer.createObject();
+
+void setup()
 {
   Serial.begin(115200);
   delay(10);
-  
-  for(int i = 0; i < LED_COUNT; i++)    
+
+  for(int i = 0; i < LED_COUNT; i++)
     pinMode(ledPin[i], OUTPUT);           //sets all led pins as output
 
   setupWifi();    //setting up and connecting to wifi
-  delay(10); 
+  delay(10);
+  Serial.println(bufferLen);
 }
 
-void loop() 
-{ 
-  incrementLights();    //if led final values have changed, this funciton fades lights up or down
-  
+void loop()
+{
+  //incrementLights();    //if led final values have changed, this funciton fades lights up or down
+
   //-----------Handles HTTP Requests-----------
   WiFiClient client = server.available();   //checking for client connection
-  if (!client) 
+  if (!client)
     return;   //restarts loop function
 
+  currTime = millis();
   //waits until the client sends data
-  Serial.println("new client connection");
-  Serial.println("");
-  int unavailableCount = 0;     //used to wait count while client is unavailable
+  unavailableCount = 0;
   while(!client.available() && unavailableCount < 9000)
   {
     unavailableCount++;
     delay(1);
-    Serial.print(".");
+    //Serial.print("...");
   }
-  Serial.println(unavailableCount);
-
+  //request handling
   String inRequest = client.readStringUntil('\r');  //reads inRequest until end of line
-  inRequest.toUpperCase();    //shifts string to upper letters to increase useablity 
-  Serial.print("Incomming request:  ");
-  Serial.println(inRequest);
+  inRequest.toUpperCase();    //shifts string to upper letters to increase useablity
+  Serial.print("incoming request:\t");
   reactToRequest(inRequest);  //reacts to information sent from client
+  delay(10);
+  //--------Creating JSON------------
+  
 
-
+  root["red"] = ledFadeTo[0];
+  root["green"] = ledFadeTo[1];
+  root["blue"] = ledFadeTo[2];
+  
+  char jsonBuffer[root.measureLength()+1];
+  root.printTo(jsonBuffer, sizeof(jsonBuffer));
   //----------HTTP Headers-------------------
   client.flush();   //clears data from client
   client.println("HTTP/1.1 200 OK");
-  client.println("Content-Type: text/html");
-  client.println("");   //always needs to be here to avoid error
-  client.println("<!DOCTYPE HTML>");
-  client.println("<html>");
-  
-  //-----------Web Page Visible When Visiting ESP8266's Local IP-----------
-  client.print("Current LED values RGB: ");
-  for(int i = 0; i < LED_COUNT; i++)
-  {
-    client.print(ledFadeTo[i]);
-    if(i != LED_COUNT - 1)    
-      client.print(",");
-  }
-  client.print("<br/>");
+  client.println("Content-Type: application/json");
+  client.println("");
+  root.printTo(Serial);
+  Serial.println();
+  root.printTo(client);
+  // client.println("Content-Type: text/html");
+  // client.println("");   //always needs to be here to avoid error
+  // client.println("<!DOCTYPE HTML><html>");
+  // client.println("<head><title>LedStrip</title></head>");
+  // client.println("<body>");
+  //
+  // //-----------Web Page Visible When Visiting ESP8266's Local IP-----------
+  // client.print("Current LED values RGB: ");
+  // for(int i = 0; i < LED_COUNT; i++)
+  // {
+  //   client.print(ledFadeTo[i]);
+  //   if(i != LED_COUNT - 1)
+  //     client.print(",");
+  // }
+  // client.print("</body></html>");
 
   incrementLights();
 }
 
 void reactToRequest(String inRequest)
 {
+  if (inRequest.indexOf("/FAVICON.ICO") != -1) //return if is a FAVICON
+    return;
+  Serial.println(inRequest);
   //turns off all lights
-  if (inRequest.indexOf("/LED=OFF") != -1)    
+  if (inRequest.indexOf("/LED=OFF") != -1)
   {
     for(int i = 0; i < LED_COUNT; i++)
-      ledFadeTo[i] = 0;  
+      ledFadeTo[i] = 0;
   }
 
   if(inRequest.indexOf("/ELWIRE") != -1)
   {
-    analogWrite(ewPin, ewStatus ? 0 : 255);
+
     ewStatus = !ewStatus;
   }
   //manually sets all incomming colors as colors the microcontroller is going to fade towards
@@ -107,18 +130,18 @@ void reactToRequest(String inRequest)
     valueIndex[0] = inRequest.indexOf("R=");
     valueIndex[1] = inRequest.indexOf("G=");
     valueIndex[2] = inRequest.indexOf("B=");
-    
+
     //saves invalues as led values to fade towards
     for(int i = 0; i < LED_COUNT; i++)
       ledFadeTo[i] = checkReadVal(inRequest.substring(valueIndex[i] + 2, valueIndex[i] + 5).toInt());
   }
-  
+
   //slowly fades RGB values for ambience
   if(inRequest.indexOf("/LED=FADE") != -1)
   {
     //TODO fade
   }
-  
+
 }//end of function reactToRequest
 
 int checkReadVal(int inVal) //check if are valid rgb values
@@ -132,24 +155,24 @@ int checkReadVal(int inVal) //check if are valid rgb values
 }
 
 void incrementLights()    //if final led values have changed this funciton fades lights up or down
-{ 
+{
   bool isChanged = false;
   for(int i = 0; i < LED_COUNT; i++) //if value are changed
     if (ledCurrentVal[i] != ledFadeTo[i]){
       isChanged = true;
       break;
     }
-   
+
   if (isChanged){ //if value has changed, fade to value for each color
     float fadeTime = 1020; //total ms to fade
     int deltaValue[LED_COUNT] = {0,0,0};
     float incrementValue[LED_COUNT] = {0,0,0};
     float fadeValue[LED_COUNT] = {ledCurrentVal[0],ledCurrentVal[1],ledCurrentVal[2]};
-    
+
     for(int i = 0; i < LED_COUNT; i++){ //calculate how much increment value each time
       deltaValue[i] = (ledFadeTo[i]) - (ledCurrentVal[i]);
       incrementValue[i] = deltaValue[i]/fadeTime;
-    } 
+    }
     for(int i = 0; i < fadeTime; i++){ //fade to value
       for(int j = 0; j < LED_COUNT; j++){
         fadeValue[j] += incrementValue[j];//increment value
@@ -157,9 +180,13 @@ void incrementLights()    //if final led values have changed this funciton fades
       }
       delay(1);
     }
-    Serial.println("\n******");
     for(int i = 0; i < LED_COUNT; i++){ //final value
-      ledCurrentVal[i] = ledFadeTo[i];  
+      if(abs(ledFadeTo[i] - fadeValue[i]) > 10){
+        Serial.println("Error in fading to value");
+        Serial.print(fadeValue[i]);
+        Serial.println(ledFadeTo[i]);
+      }
+      ledCurrentVal[i] = ledFadeTo[i];
       analogWrite(ledPin[i], ledCurrentVal[i]);
     }
   }
@@ -173,18 +200,24 @@ void setupWifi()
   WiFi.config(ip, gateway, subnet); //static configuration
   WiFi.begin(ssid, wifiPass);   //connecting to wifi
   WiFi.mode(WIFI_STA);
-  
-  while (WiFi.status() != WL_CONNECTED) 
+
+  while (WiFi.status() != WL_CONNECTED)
   {
     Serial.print(".");
     delay(500);
   }
   Serial.println("\nWiFi connected");
-  
+
   server.begin();   //starting server on ESP8266
   Serial.println("Server started");
   Serial.print("Local IP adress: ");
   Serial.println(WiFi.localIP());
   Serial.println(ip);
+  Serial.println("");
+
   //WiFi.printDiag(Serial);
+}
+
+void encodeJson(/* arguments */) {
+  /* code */
 }
