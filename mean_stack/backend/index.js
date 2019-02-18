6d09d9a1ced2c038f68486732d654309f4b68a1a
rickycraft <ricky.roveri@gmail.com>
2019-02-18 19:56:40 +0100

    nodejs added post request for lux and rgb

--- mean_stack/backend/index.js
+++ mean_stack/backend/index.js
@@ -9,21 +9,49 @@ const baseUrl = 'http://192.168.1.220';
 app.use(cors());
 app.use(express.json());
 
+var led = {
+    red: 0,
+    green: 0,
+    blu: 0,
+    lux: 0,
+};
+
+var elwire = false;
+
 app.get('/status', async (req, res) => {
     await request.get(baseUrl+'/status', (err,response,body) => {
         if (err) {
             console.log(err);
             res.sendStatus(500);
         } else {
-            res.status(200).send(JSON.parse(body));
+            led = JSON.parse(body);
+            res.status(200).send(body);
         }
     })
     console.log("status requested");
 })
 
-app.post('/rgb', async (req,res) => {
+app.post('/rgbl', (req,res) => {
+    led = req.body;
+    setLed();
+    
+})
+
+app.post('/lux', async (req, res) => {
+    led.lux = req.body;
+    setLed();
+})
+
+app.post('/rgb', async (req, res) => {
+    led.red = req.body.red;
+    led.green = req.body.green;
+    led.blu = req.body.blu;
+    setLed();
+})
+
+async function setLed(){
     await request.get(
-        baseUrl+'/led/&'+checkColor(req.body.red)+':'+checkColor(req.body.green)+':'+checkColor(req.body.blu)+':'+checkColor(req.body.lux),
+        baseUrl+'/led/&'+checkColor(led.red)+':'+checkColor(led.green)+':'+checkColor(led.blu)+':'+checkColor(led.lux),
         (err, response, body) => { //get request
             if (response.statusCode == 500) {
                 res.sendStatus(500); 
@@ -33,7 +61,8 @@ app.post('/rgb', async (req,res) => {
            } 
        });
     console.log("rgb request");
-})
+    console.log(led);
+}
 
 function checkColor(color){
     if (color<10){
@@ -43,8 +72,8 @@ function checkColor(color){
     }
 }
 
-app.post('/elwire', (req,res) => {
-    request.get(baseUrl+'/elwire', (err,response,body) => {
+app.post('/elwire', async (req,res) => {
+    await request.get(baseUrl+'/elwire', (err,response,body) => {
         if (err) {
             console.log(err);
             res.sendStatus(500);
@@ -52,6 +81,8 @@ app.post('/elwire', (req,res) => {
             res.status(200).send(JSON.parse(body));
         }
     })
+    elwire = !elwire;
+    console.log(elwire);
 })
 
 app.listen(port);
