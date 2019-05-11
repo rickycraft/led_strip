0838010a6fa8a5f1396bf7c2afed8d7fc8bb1d27
rick <ricky.roveri@gmail.com>
2019-05-11 12:47:31 +0200

    frontend, chart update data

--- frontend/src/app/chart/chart.component.ts
+++ frontend/src/app/chart/chart.component.ts
@@ -4,7 +4,7 @@ import { Color, Label, BaseChartDirective } from 'ng2-charts';
 import { WeatherService } from '../services/weather.service';
 
 @Component({
-  selector: 'chart',
+  selector: 'app-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.css'],
 })
@@ -13,9 +13,9 @@ export class ChartComponent implements OnInit {
     // weatherService.getAvgH(new Date('2019-04-22'));
   }
   public chartLabels: Label[] = [];
-  public t_chartData: ChartDataSets[] = [
+  public t_data: ChartDataSets[] = [
     {
-      data: new Array<number>(24),
+      data: [10, 20],
       label: 'temp',
     },
   ];
@@ -25,14 +25,15 @@ export class ChartComponent implements OnInit {
       backgroundColor: 'rgba(255,0,0,0.5)',
     },
   ];
-  public t_chartOptions: ChartOptions = {
+  public t_options: ChartOptions = {
+    responsive: true,
     aspectRatio: 1.7,
     scales: {
       yAxes: [
         {
           ticks: {
-            suggestedMin: this.getEdge(this.t_chartData[0].data).min,
-            suggestedMax: this.getEdge(this.t_chartData[0].data).max,
+          suggestedMin: 10,
+          suggestedMax: 30, // this.getEdge(this.t_data[0].data).max,
           },
         },
       ],
@@ -40,25 +41,31 @@ export class ChartComponent implements OnInit {
   };
   @ViewChild(BaseChartDirective) chart: BaseChartDirective;
 
-  getEdge(list: any[]): any {
-    const newData: any[] = list.map(val => {
-      return parseInt(val);
+  getEdge(arr: any[]): any {
+    const list: number[] = arr.map(val => {
+      return parseInt(val, 10);
     });
-    const min: number = parseInt( Math.min(...newData).toFixed(0) ) - 2;
-    const max: number = parseInt( Math.max(...newData).toFixed(0) ) + 2;
+    const min: number = parseInt( Math.min(...list).toFixed(0), 10 ) - 2;
+    const max: number = parseInt( Math.max(...list).toFixed(0), 10 ) + 2;
+    console.log(min);
     return { min: min, max: max };
   }
 
   ngOnInit() {
     for (let i = 0; i < 24; i++) {
       this.chartLabels.push(i.toString() + ':00');
-      this.t_chartData[0].data[i] = 20;
     }
-    console.log(this.t_chartData[0].data);
-    this.chart.update();
   }
 
-  updateChart() {
+  public updateChart(list: number[]) {
+    // test values
+    list = [21, 21, 22, 23, 24, 23, 22, 21, 20, 21, 22, 23, 24, 23, 22, 21, 20, 22, 25, 23, 21, 20, 22, 25];
+    list.forEach( (val, i) => {
+      this.t_data[0].data[i] = val;
+    });
+    // this.t_options.scales.yAxes[0].ticks.min
+    // this.chart.options.scales.yAxes[0].ticks.suggestedMax = 28;
     this.chart.update();
+    console.log('updated');
   }
 }
